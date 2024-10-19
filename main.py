a3e634ad5d94bf90fd5c2affa7c2f18270f29846
Bill <iilmubarak.06@gmail.com>
2024-10-19 08:35:09 +0700

    "finished making barebone"

--- main.py
+++ main.py
@@ -7,158 +7,144 @@ import json
 
 data_path = os.path.dirname(__file__) + "/data.json"
 
-def __init__():
+def initiate_db():
     if not os.path.exists(data_path):
         with open(data_path, "w") as file:
             json.dump([], file)
 
+def get_status():
+    try:
+        return sys.argv[2]
+    except IndexError:
+        return "all"
+
+def get_current_time():
+    return datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+
+def load():
+    with open(data_path, "r")as file:
+        data = json.load(file)
+
+    return data
+
+def save(data):
+    with open(data_path, "w") as file:
+        json.dump(data, file, indent=4)
+
 def get_id():
-    with open(data_path, "r") as file:
-        try:
-            data = json.load(file)
-        except json.JSONDecodeError:
-            data = []
-        list_of_ids = []
-        for task in data:
-            list_of_ids.append(task["id"])
-    for i in range(0, 100):
+    list_of_ids = []
+    current_data = load()
+
+    for task in current_data:
+        list_of_ids.append(task["id"])
+
+    for i in range(1, 101):
         if i not in list_of_ids:
             return i
             break
 
-def add():
-    with open(data_path, "r") as file:        
-        try:
-            current_data = json.load(file)
-        except json.JSONDecodeError:
-            current_data = []
-    data = {}
-    data["id"] = get_id()
-    data["description"] = sys.argv[2]
-    data["status"] = "todo"
-    data["created_at"] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-    data["updated_at"] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-    current_data.append(data)
-    with open(data_path, "w") as file:
-        json.dump(current_data, file, indent=4)
+def add(desc):
+    current_data = load()
 
-def update():
-    with open(data_path, "r") as file:
-        try:
-            current_data = json.load(file)
-        except json.JSONDecodeError:
-            current_data = []
-    for task in current_data:
-        if task["id"] == int(sys.argv[2]):
-            task["description"] = sys.argv[3]
-            task["updated_at"] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-            break
-    with open(data_path, "w") as file:
-        json.dump(current_data, file, indent=4)
+    if len(current_data) == 100:
+        print("Database is full, please remove some task.")
 
-def delete():
-    with open(data_path, "r") as file:
-        try:
-            current_data = json.load(file)
-        except json.JSONDecodeError:
-            pass
-    for task in current_data:
-        if task["id"] == int(sys.argv[2]):
-            current_data.remove(task)
-            with open(data_path, "w") as file:
-                json.dump(current_data, file, indent=4)
+    else:
+        data = {}
+        data["id"] = get_id()
+        data["description"] = desc
+        data["status"] = "todo"
+        data["created_at"] = get_current_time()
+        data["updated_at"] = get_current_time()
+        current_data.append(data)
+        save(current_data)
 
-def mark():
-    with open(data_path, "r") as file:
-        try:
-            current_data = json.load(file)
-        except json.JSONDecodeError:
-            pass
+def update(id, desc):
+    current_data = load()
+
+    try:
+        for task in current_data:
+            if task["id"] == id:
+                task["description"] = desc
+                task["updated_at"] = get_current_time()
+                break
+
+        save(current_data)
+
+    except IndexError:
+        print("No such file is found.")
+
+def delete(id):
+    current_data = load()
+
+    try:
+        for task in current_data:
+            if task["id"] == id:
+                current_data.remove(task)
+
+        save(current_data)
+
+    except IndexError:
+        print("No such file is found.")
+
+def mark(id, stat):
+    stat = stat[0].lower()
+    current_data = load()
+
     for task in current_data:
-        if task["id"] == int(sys.argv[2]):
-            task["status"] = sys.argv[3]
-    with open(data_path, "w") as file:
-            json.dump(current_data, file, indent=4)
+        if task["id"] == id:
+            match stat:
+                case "todo": task["status"] = "todo"
+                case "done": task["status"] = "done"
+                case "in": task["status"] = "in-progress"
+                case _: print("invalid status.")
+
+    save(current_data)
         
-def list_all():
-    with open(data_path, "r")as file:
-        current_data = json.load(file)
-    index = "Id"
-    desc = "Description"
-    stat = "Status"
-    ca = "Created At"
-    ua = "Updated At"
-    print("="*91)
-    print(f"| {index.center(2):2} | {desc.center(30):30} | {stat.center(11):11} | {ca.center(16):16} | {ua.center(16):16} |")
-    print("-"*91)
-    for data in current_data:
-        index = data["id"]
-        desc = data["description"]
-        stat = data["status"]
-        ca = data["created_at"]
-        cu = data["updated_at"]
-        print(f"| {index:2} | {desc:30} | {stat:11} | {ca:16} | {ca:16} |")
-    print("="*91)
+def task_list(status):
+    current_data = load()
 
-def list_done():
-    with open(data_path, "r")as file:
-        current_data = json.load(file)
     index = "Id"
     desc = "Description"
     stat = "Status"
-    ca = "Created At"
-    ua = "Updated At"
-    print("="*94)
-    print(f"| {index.center(2):2} | {desc:30} | {stat:11} | {ca:14} | {ua:14} |")
-    print("-"*94)
+    created = "Created At"
+    updated = "Updated At"
+
+    print("="*92)
+    print(f"| {index.center(3):3} | {desc.center(30):30} | {stat.center(11):11} | {created.center(16):16} | {updated.center(16):16} |")
+    print("-"*92)
+
     for data in current_data:
-        if data["status"] == "done":
+        if status == "all":
             index = data["id"]
             desc = data["description"]
             stat = data["status"]
-            ca = data["created_at"]
-            cu = data["updated_at"]
-            print(f"| {index:2} | {desc:30} | {stat:11} | {ca:14} | {cu:14} |")
+            created = data["created_at"]
+            updated = data["updated_at"]
+        else:
+            if data["status"] == status:
+                index = data["id"]
+                desc = data["description"]
+                stat = data["status"]
+                created = data["created_at"]
+                updated = data["updated_at"]
+            else:
+                continue
 
-def list_not_done():
-    with open(data_path, "r")as file:
-        current_data = json.load(file)
-    index = "Id"
-    desc = "Description"
-    stat = "Status"
-    ca = "Created At"
-    ua = "Updated At"
-    print("="*94)
-    print(f"| {index.center(2):2} | {desc:30} | {stat:11} | {ca:14} | {ua:14} |")
-    print("-"*94)
-    for data in current_data:
-        if data["status"] == "todo":
-            index = data["id"]
-            desc = data["description"]
-            stat = data["status"]
-            ca = data["created_at"]
-            cu = data["updated_at"]
-            print(f"| {index:2} | {desc:30} | {stat:11} | {ca:14} | {cu:14} |")
+        print(f"| {str(index).center(3):3} | {desc:30} | {stat:11} | {created:16} | {updated:16} |")
+    print("="*92)
 
-def list_in_progress():
-    with open(data_path, "r")as file:
-        current_data = json.load(file)
-    index = "Id"
-    desc = "Description"
-    stat = "Status"
-    ca = "Created At"
-    ua = "Updated At"
-    print("="*91)
-    print(f"| {index.center(2):2} | {desc:30} | {stat:11} | {ca:16} | {ua:16} |")
-    print("-"*91)
-    for data in current_data:
-        if data["status"] == "in-progress":
-            index = data["id"]
-            desc = data["description"]
-            stat = data["status"]
-            ca = data["created_at"]
-            cu = data["updated_at"]
-            print(f"| {index:2} | {desc:30} | {stat:11} | {ca:16} | {cu:16} |")
-    print("="*91)
+#driver
 
-list_all()
+initiate_db()
+
+option = sys.argv[1].split("-")
+
+match option[0]:
+    case "add": add(sys.argv[2])
+    case "update": update(int(sys.argv[2]), sys.argv[3])
+    case "mark": mark(int(sys.argv[2]), sys.argv[3].split("-"))
+    case "delete": delete(int(sys.argv[2]))
+    case "list": task_list(get_status())
+    case _: print("invalid command.")
+    
