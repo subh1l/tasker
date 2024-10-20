a6e6259203111d550203a6fe15ee9bb637cb4739
Bill <iilmubarak.06@gmail.com>
2024-10-20 13:36:47 +0700

    created README.MD

---
+++ README.md
@@ -0,0 +1,51 @@
+o# Tasker: CLI Task Tracker App
+Tasker is a task tracker application based on command-line interface. This application allows users to add, delete, update, and list tasks using various commands. It provides a straightforward way to interact with a task database using a simple CLI interface, making it an ideal tool for both personal and professional task tracking.
+
+## Features
+Tasker have various function to help users track and manage their task:
+- **Add a task**: Users can add task by providing description. each newly added task will be given unique ID and set to "todo" status.
+- **update a task**: Users can change their task description by providing the task ID and the new description.
+- **Mark a task**: Users can set their task status either as "done" or "in-progress" by providing the task ID.
+- **Delete a task**: Users can remove task from the list by providing task ID.
+- **List a task**: Users can list all their tasks and filter them based on the status by providing the status.
+
+## Installation & Usage
+1. Installation
+    To install the application clone the git repository using `git clone https://github.com/subh1l/tasker.git`
+
+2. Usage
+    1. Open your terminal.
+
+    2. Go to the directory where the main file is.
+
+    3. run the project.
+
+        - Adding task: `./tasker.py add "task description"`
+            Example: `/tasker.py add "wash the dishes."`   
+            Output: New task with correspoding ID added to the list.
+
+        - Updating task: `./tasker.py update task_ID "new task description"`
+            Example: `./tasker.py update 1 "wash the dishes and cook dinner."`
+            Output: Description of task with correspoding ID will change to the new description.
+
+        - Marking task: `./tasker.py mark* (task ID)`
+            *Valid argument: "-done" or "-in-progress"
+            Example: `./tasker.py mark-done 1"`
+            Output: Status of task with corresponding ID will change to the new status.
+
+        - Deleting task: `./tasker.py delete (task ID)`
+            Example: `./tasker.py delete 1`
+            Output: Task with corresponding ID is deleted.
+        
+        - Listing task: `./tasker.py list *"`
+            *Valid command: "todo", "in-progress", "done", or leave it empty to show all task
+            Example: 
+            - `./tasker.py list`
+            - `./tasker.py list todo`
+            Output:
+            - List all task in database
+            - List all task with status todo in database.
+            
+        
+## Project Link
+to see more information click this [link](https://roadmap.sh/projects/task-tracker)
--- tasker.py
+++ tasker.py
@@ -1,18 +1,154 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+import sys
+import datetime
+import json
 
-import func
 
-func.initiate_db()
+data_path = os.path.dirname(__file__) + "/data.json"
+
+def initiate_db():
+    if not os.path.exists(data_path):
+        with open(data_path, "w") as file:
+            json.dump([], file)
+
+def get_status():
+    try:
+        return sys.argv[2]
+    except IndexError:
+        return "all"
+
+def get_current_time():
+    return datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+
+def load():
+    with open(data_path, "r")as file:
+        data = json.load(file)
+
+    return data
+
+def save(data):
+    with open(data_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+def get_id():
+    list_of_ids = []
+    current_data = load()
+
+    for task in current_data:
+        list_of_ids.append(task["id"])
+
+    for i in range(1, 101):
+        if i not in list_of_ids:
+            return i
+            break
+
+def desc_limit(input):
+    space_num = 30 - len(input)
+    return input + (space_num*" ")
+
+def add(desc):
+    current_data = load()
+
+    if len(current_data) == 100:
+        print("Database is full, please remove some task.")
+
+    else:
+        data = {}
+        data["id"] = get_id()
+        data["description"] = desc
+        data["status"] = "todo"
+        data["created_at"] = get_current_time()
+        data["updated_at"] = get_current_time()
+        current_data.append(data)
+        save(current_data)
+
+def update(id, desc):
+    current_data = load()
+
+    try:
+        for task in current_data:
+            if task["id"] == id:
+                task["description"] = desc
+                task["updated_at"] = get_current_time()
+                break
+
+        save(current_data)
+
+    except IndexError:
+        print("No such file is found.")
+
+def delete(id):
+    current_data = load()
+
+    try:
+        for task in current_data:
+            if task["id"] == id:
+                current_data.remove(task)
+
+        save(current_data)
+
+    except IndexError:
+        print("No such file is found.")
+
+def mark(id, stat):
+    stat = stat[1].lower()
+    current_data = load()
+
+    for task in current_data:
+        if task["id"] == id:
+            match stat:
+                case "done": task["status"] = "done"
+                case "in": task["status"] = "in-progress"
+                case _: print("invalid status.")
+
+    save(current_data)
+
+def task_list(status):
+    current_data = load()
+
+    index = "ID"
+    desc = "Description"
+    stat = "Status"
+    created = "Created At"
+    updated = "Updated At"
+
+    print("="*92)
+    print(f"| {index.center(3):3} | {desc.center(30):30} | {stat.center(11):11} | {created.center(16):16} | {updated.center(16):16} |")
+    print("-"*92)
+
+    for data in current_data:
+        if status == "all":
+            index = data["id"]
+            desc = desc_limit(data["description"])
+            stat = data["status"]
+            created = data["created_at"]
+            updated = data["updated_at"]
+        else:
+            if data["status"] == status:
+                index = data["id"]
+                desc = desc_limit(data["description"])
+                stat = data["status"]
+                created = data["created_at"]
+                updated = data["updated_at"]
+            else:
+                continue
+
+        print(f"| {str(index).center(3):3} | {desc:.30} | {stat:11} | {created:16} | {updated:16} |")
+    print("="*92)
+
+
+initiate_db()
 
 option = sys.argv[1].split("-")
 
 match option[0]:
-    case "add": func.add(sys.argv[2])
-    case "update": func.update(int(sys.argv[2]), sys.argv[3])
-    case "mark": func.mark(int(sys.argv[2]), sys.argv[1].split("-"))
-    case "delete": func.delete(int(sys.argv[2]))
-    case "list": func.task_list(func.get_status())
+    case "add": add(sys.argv[2])
+    case "update": update(int(sys.argv[2]), sys.argv[3])
+    case "mark": mark(int(sys.argv[2]), sys.argv[1].split("-"))
+    case "delete": delete(int(sys.argv[2]))
+    case "list": task_list(get_status())
     case _: print("invalid command.")
     
